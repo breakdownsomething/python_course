1484c8836ccf85b4e21d9e1c5f651909e1fd63c6
Danil Matessov <danil.matessov@gmail.com>
2020-05-01 17:54:25 +0600

    q

--- decorator.py
+++ decorator.py
@@ -30,28 +30,74 @@ class Hero:
 
 class AbstractEffect(Hero, ABC):
     def __init__(self, obj):
-        super().__init__()
-        self.obj = obj
+        self.base = obj
 
+    def get_positive_effects(self):
+        return self.base.get_positive_effects()
+
+    def get_negative_effects(self):
+        return self.base.get_negative_effects()
+
+    def get_stats(self):
+        return self.base.get_stats()
+
+    def add_positive(self, effect):
+        try:
+            self.base.positive_effects.append(effect)
+        except AttributeError:
+            self.base.add_positive(effect)
+
+    def add_negative(self, effect):
+        try:
+            self.base.negative_effects.append(effect)
+        except AttributeError:
+            self.base.add_negative(effect)
 
 
 class AbstractPositive(AbstractEffect):
-    pass
+    def __init__(self, obj, effect):
+        super().__init__(obj)
+        self.add_positive(effect)
+
 
 class AbstractNegative(AbstractEffect):
-    pass
+    def __init__(self, obj, effect):
+        super().__init__(obj)
+        self.add_negative(effect)
+
 
 class Berserk(AbstractPositive):
-    pass
+    def __init__(self, obj):
+        super().__init__(obj, self.__class__.__name__)
+
 
 class Blessing(AbstractPositive):
-    pass
+    def __init__(self, obj):
+        super().__init__(obj, self.__class__.__name__)
+
 
 class Weakness(AbstractNegative):
-    pass
+    def __init__(self, obj):
+        super().__init__(obj, self.__class__.__name__)
+
 
 class Curse(AbstractNegative):
-    pass
+    def __init__(self, obj):
+        super().__init__(obj, self.__class__.__name__)
+
 
 class EvilEye(AbstractNegative):
-    pass
+    def __init__(self, obj):
+        super().__init__(obj, self.__class__.__name__)
+
+
+h = Hero()
+m1 = Berserk(h)
+m2 = Berserk(m1)
+m3 = EvilEye(m2)
+print(m3.get_positive_effects())
+print(m3.get_negative_effects())
+
+m3.base = m1
+print(m3.get_positive_effects())
+print(m3.get_negative_effects())
